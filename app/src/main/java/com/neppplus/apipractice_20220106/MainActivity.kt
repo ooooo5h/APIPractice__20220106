dd241611b79e71cff4b33a949ca0731f899a666e
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 10:52:29 +0900

    로그인 기능 응답 처리   - 토스트로 성공/실패만 나뉘어서 띄웠음

--- app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
@@ -2,9 +2,14 @@ package com.neppplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.neppplus.apipractice_20220106.api.APIList
 import com.neppplus.apipractice_20220106.api.ServerAPI
+import com.neppplus.apipractice_20220106.models.BasicResponse
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,33 @@ class MainActivity : AppCompatActivity() {
             val apiList = retrofit.create(APIList::class.java)  // 연결도구와 기능 목록을 결합한 객체를 생성함
 
 //            실제 로그인 기능 호출(Request를 보낸다)
-            apiList.postRequestLogin(inputEmail, inputPw)
+            apiList.postRequestLogin(inputEmail, inputPw).enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    
+//                    로그인 성공/실패던, 응답 자체가 돌아온 경우
+//                    서버가 정상 동작한다는 뜻 
+                    
+//                    성공/실패 경우가 나뉨
+                    if (response.isSuccessful){
+//                        로그인 까지 성공 (아이디와 비밀번호 모두 일치)
+                        Toast.makeText(this@MainActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+//                        로그인 실패 (아이디 or 비밀번호 불일치)
+                        Toast.makeText(this@MainActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                    
+//                    서버 자체가 동작을 안한다는 뜻(서버 연결 자체가 실패)
+                    
+                }
+
+            })
 
         }
 
