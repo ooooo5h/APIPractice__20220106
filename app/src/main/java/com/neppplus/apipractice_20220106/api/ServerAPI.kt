f01f7dbaaf28199e260a3075a8ba050f3882b8a9
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 10:00:22 +0900

    Retrofit 객체를 생성할 준비중   - 어느 서버에 연결할건지 주소 셋팅 BASE_URL   - 객체를 담게될 변수를 생성했는데 직접 접근은 못하게 막음   - 객체를 받아올 때 사용할 함수를 생성했음

--- app/src/main/java/com/neppplus/apipractice_20220106/api/ServerAPI.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/api/ServerAPI.kt
@@ -1,6 +1,6 @@
 package com.neppplus.apipractice_20220106.api
 
-import android.util.Log
+import retrofit2.Retrofit
 
 class ServerAPI {
 
@@ -9,9 +9,22 @@ class ServerAPI {
 //    클래스이름.기능()  => @classmethod에 대응되는 기능을 활용하려고 하는 것
 
     companion object {
-       fun test() {
-           Log.d("서버API클래스", "테스트기능")
-       }
+//        이 companion object {}안에 적는 변수와 함수들은, ServerAPI.변수/함수명 으로 바로 활용(노출)이 가능하다
+
+//        val BASE_URL = "https://api.gudoc.in"
+
+        private val BASE_URL = "https://api.gudoc.in"  //이 변수는 노출안되고, 이 클래스 내부에서만 사용할 예정 -> private 처리
+        private var retrofit : Retrofit? = null  // 서버 연결을 전담하는 변수. 기본적으로는 아직 안만든 상태기때문에 null대입
+
+//        retrofit 변수에 환경설정 + 객체화 => 가져다 쓸 수 있게 하는 기능을 함수로 제공
+        fun getRetrofit() : Retrofit {
+
+
+
+
+
+        }
 
     }
+
 }
