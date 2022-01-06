4a610af94eaf8bded688c2a15c267900409e97b9
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 10:41:01 +0900

    버튼 클릭시 입력한 이메일과 비밀번호를 변수에 저장

--- app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
@@ -2,12 +2,24 @@ package com.neppplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnLogin.setOnClickListener {
+
+//            입력한 이메일과 비밀번호를 변수에 저장
+            val inputEmail = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+
+//            서버 API의 로그인 기능에 활용해보기
+
+
+
+        }
 
 
     }
