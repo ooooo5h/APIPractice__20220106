974ac5a4090a111810aac34fc36e81e25fae34e3
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 15:34:45 +0900

    BaseActivity에서 물려줄 변수로 apiList변수 추가

--- app/src/main/java/com/neppplus/apipractice_20220106/BaseActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/BaseActivity.kt
@@ -1,6 +1,26 @@
 package com.neppplus.apipractice_20220106
 
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.neppplus.apipractice_20220106.api.APIList
+import com.neppplus.apipractice_20220106.api.ServerAPI
+import retrofit2.Retrofit
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    lateinit var retrofit : Retrofit
+    lateinit var apiList : APIList
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        이 내부의 코드는 모든 화면들의 시작지점에서 같이 공통적으로 실행되는 코드
+
+//        레트로핏 관련 설정들을 여기서 진행한다(모든 화면들이 자동으로 실행되도록)
+        retrofit = ServerAPI.getRetrofit()
+        apiList = retrofit.create(APIList::class.java)
+
+
+    }
 }
