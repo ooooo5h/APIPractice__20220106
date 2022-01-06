5e42b6e50a8dacaa9688b0c40da14a4310e0ff71
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 15:41:09 +0900

    로그인 성공시 메인으로 이동   - this@ 짜잉나서 BaseActicity에서 mContext에 this를 담아주고 그 변수롤 사용

--- app/src/main/java/com/neppplus/apipractice_20220106/LoginActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/LoginActivity.kt
@@ -44,7 +44,12 @@ class LoginActivity : BaseActivity() {
                         val loginUserNick = br.data.user.nick_name
 
                         Toast.makeText(this@LoginActivity, "${loginUserNick}님, 환영합니다!", Toast.LENGTH_SHORT).show()
-                        
+
+//                        메인 화면으로 이동
+                        val myIntent = Intent(mContext, MainActivity::class.java)
+                        startActivity(myIntent)
+
+                        finish()
                         
                     }
                     else {
