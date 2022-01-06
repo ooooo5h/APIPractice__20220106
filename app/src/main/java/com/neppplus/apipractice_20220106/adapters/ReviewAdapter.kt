980aa5771f141c884e8f16a0041af322415942a0
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 16:02:27 +0900

    리뷰 어댑터 - Ctrl C / V 이용 작성

---
+++ app/src/main/java/com/neppplus/apipractice_20220106/adapters/ReviewAdapter.kt
@@ -0,0 +1,52 @@
+package com.neppplus.apipractice_20220106.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.apipractice_20220106.R
+import com.neppplus.apipractice_20220106.models.ReviewData
+
+
+class ReviewAdapter(
+    val mContext: Context,
+    val mList: List<ReviewData>,
+) : RecyclerView.Adapter<ReviewAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+
+
+        fun bind(data: ReviewData) {
+
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.review_list_item, parent, false)
+
+        return MyViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+
+        holder.bind(mList[position])
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return mList.size
+
+    }
+
+}
