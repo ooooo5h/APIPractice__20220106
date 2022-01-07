62049c3cd41cd9b979ff937e8e2cbbd30d6a8286
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 10:11:49 +0900

    닉네임 / 프사 실제 파싱, 반영   - 이미 파싱되어있는 구조를 재활용

--- app/src/main/java/com/neppplus/apipractice_20220106/MyProfileActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/MyProfileActivity.kt
@@ -2,8 +2,10 @@ package com.neppplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.neppplus.apipractice_20220106.models.BasicResponse
 import com.neppplus.apipractice_20220106.utils.ContextUtil
+import kotlinx.android.synthetic.main.activity_my_profile.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,6 +22,21 @@ class MyProfileActivity : BaseActivity() {
 
         apiList.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object : Callback<BasicResponse>{
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful) {
+
+//                BasicResponse를 br변수로 담아두자
+                    val br = response.body()!!
+
+//                    사용자가 누구인지? 변수에 담자
+                    val myInfo = br.data.user
+
+//                    UI에 닉네임 등 반영해보기
+                    txtMyNickname.text = myInfo.nick_name
+                    Glide.with(mContext).load(myInfo.profile_img).into(imgMyProfile)
+
+                }
+
 
             }
 
