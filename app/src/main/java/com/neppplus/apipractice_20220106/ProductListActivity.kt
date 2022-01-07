2588614b28aee68a0eb0a94c1389999fa74c743c
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 11:11:38 +0900

    상품 목록을 담을 준비 중   - ArrayList 생성   - 모든 상품 목록 API 호출

--- app/src/main/java/com/neppplus/apipractice_20220106/ProductListActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/ProductListActivity.kt
@@ -1,11 +1,34 @@
 package com.neppplus.apipractice_20220106
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.apipractice_20220106.models.BasicResponse
+import com.neppplus.apipractice_20220106.models.ProductData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProductListActivity : BaseActivity() {
+
+    val mProductList = ArrayList<ProductData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
+
+        apiList.getRequestAllProduct().enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+
+        })
+
+
     }
 }
