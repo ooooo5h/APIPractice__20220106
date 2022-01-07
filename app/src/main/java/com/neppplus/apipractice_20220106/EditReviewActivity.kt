0e791c2e582683012cfb7d94bffabc15f4490953
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 12:11:43 +0900

    실제 리뷰 등록 API 호출

--- app/src/main/java/com/neppplus/apipractice_20220106/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/EditReviewActivity.kt
@@ -2,8 +2,13 @@ package com.neppplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.neppplus.apipractice_20220106.models.BasicResponse
 import com.neppplus.apipractice_20220106.models.ProductData
 import kotlinx.android.synthetic.main.activity_edit_review.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditReviewActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +25,33 @@ class EditReviewActivity : BaseActivity() {
             val inputScore = ratingBar.rating
 
 //            서버에 전달하자 POST / review로
+            apiList.postRequestSubmitReview(
+                product.id,
+                inputTitle,
+                inputContent,
+                inputScore,
+                ""
+            ).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                    if (response.isSuccessful) {
+
+//                        토스트만 띄우고 화면을 종료하자
+                        Toast.makeText(mContext, "리뷰 등록에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+
+            })
 
 
 
