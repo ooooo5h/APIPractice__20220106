29116d37674826a96084a753646c1b1315ee424f
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 11:08:10 +0900

    상품 목록을 뿌려주는 어댑터 추가

---
+++ app/src/main/java/com/neppplus/apipractice_20220106/adapters/ProductAdapter.kt
@@ -0,0 +1,56 @@
+package com.neppplus.apipractice_20220106.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.neppplus.apipractice_20220106.R
+import com.neppplus.apipractice_20220106.models.ProductData
+import com.neppplus.apipractice_20220106.models.ReviewData
+import com.neppplus.apipractice_20220106.models.StoreData
+import org.w3c.dom.Text
+
+
+class ProductAdapter(
+    val mContext: Context,
+    val mList: List<ProductData>,
+) : RecyclerView.Adapter<ProductAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+
+
+        fun bind(data: ProductData) {
+
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.product_list_item, parent, false)
+
+        return MyViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+
+        holder.bind(mList[position])
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return mList.size
+
+    }
+
+}
