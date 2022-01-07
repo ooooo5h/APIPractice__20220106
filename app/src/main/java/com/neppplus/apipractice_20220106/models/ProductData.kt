3901fb2a469714f4ed656378ff58804ca88b02f6
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 11:02:40 +0900

    상품 데이터 클래스 제작중   - 이미지 URL을 가져올 때, 변수명을 서버와 다르게 만들었음(SerializedName)

---
+++ app/src/main/java/com/neppplus/apipractice_20220106/models/ProductData.kt
@@ -0,0 +1,12 @@
+package com.neppplus.apipractice_20220106.models
+
+import com.google.gson.annotations.SerializedName
+
+class ProductData(
+    val id : Int,
+    val name : String,
+    val price : Int,
+    @SerializedName("image_url")
+    val imageURL : String,  // 서버가 주는 이름표와 우리가 저장하려는 변수명이 다를 때 이렇게 코드 작성
+) {
+}
