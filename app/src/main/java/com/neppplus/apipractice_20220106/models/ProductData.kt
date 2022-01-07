06c386253797e2f821ae6ebd7430a79f1c7cfcc1
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 11:05:23 +0900

    리싸이클러뷰의 데이터 클래스를 추가했음   - 서버에서 받아서 보여줄 목록 : 서버가 주는 데이터 형태를 파싱하는 클래스로 추가

--- app/src/main/java/com/neppplus/apipractice_20220106/models/ProductData.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/models/ProductData.kt
@@ -8,5 +8,6 @@ class ProductData(
     val price : Int,
     @SerializedName("image_url")
     val imageURL : String,  // 서버가 주는 이름표와 우리가 저장하려는 변수명이 다를 때 이렇게 코드 작성
+    val store : StoreData,
 ) {
 }
---
+++ app/src/main/java/com/neppplus/apipractice_20220106/models/StoreData.kt
@@ -0,0 +1,11 @@
+package com.neppplus.apipractice_20220106.models
+
+import com.google.gson.annotations.SerializedName
+
+class StoreData(
+    val id : Int,
+    val name : String,
+    @SerializedName("logo_url")
+    val logoURL : String,
+) {
+}
