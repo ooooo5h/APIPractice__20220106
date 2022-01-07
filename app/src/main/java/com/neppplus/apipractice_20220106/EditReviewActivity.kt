af744e05f650727c4241a01e50145fd1c6d9d35c
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 12:02:42 +0900

    리뷰 등록 이벤트 처리 중

--- app/src/main/java/com/neppplus/apipractice_20220106/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/EditReviewActivity.kt
@@ -2,10 +2,28 @@ package com.neppplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.apipractice_20220106.models.ProductData
+import kotlinx.android.synthetic.main.activity_edit_review.*
 
 class EditReviewActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_review)
+
+//        이전 화면이 넘겨준 상품 데이터 받아오기
+        val product = intent.getSerializableExtra("product") as ProductData
+
+        btnSubmitReview.setOnClickListener {
+
+            val inputTitle = edtTitle.text.toString()
+            val inputContent = edtContent.text.toString()
+            val inputScore = ratingBar.rating
+
+//            서버에 전달하자 POST / review로
+
+
+
+
+        }
     }
 }
